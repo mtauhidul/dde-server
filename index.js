18e6e1c38769f90514bee6d2de6d81581c751f8c
mtauhidul <mislam.tauhidul@gmail.com>
2022-10-15 00:49:22 +0600

    stable state update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-require('dotenv').config;
+require('dotenv').config();
 const cluster = require('cluster');
 const os = require('os');
 const numCPUs = os.cpus().length;
@@ -7,6 +7,8 @@ const numCPUs = os.cpus().length;
 const http = require('http');
 
 const server = http.createServer(app);
+
+const port = process.env.PORT || 3001;
 
 if (cluster.isMaster) {
   for (let i = 0; i < numCPUs; i++) {
@@ -16,7 +18,7 @@ if (cluster.isMaster) {
     console.log(`worker ${worker.process.pid} died`);
   });
 } else {
-  server.listen((port = 3001 || process.env.PORT), () => {
+  server.listen(port, () => {
     console.log(`Server ${process.pid} is running on port ${port}`);
   });
 }
