03b3f3daa4a963fab0bfc7f05cb941616d0c1b2a
mtauhidul <mislam.tauhidul@gmail.com>
2022-10-19 21:58:10 +0600

    new update

--- server/app.js
+++ server/app.js
@@ -7,11 +7,13 @@ const axios = require('axios');
 const checkStatusCode = require('./utils/statusCheck');
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../client', 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
-});
+app.use(express.static(path.resolve(__dirname, '../client', 'build')));
+
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+});
 
 app.post('/api', async (req, res) => {
   const domains = req.body.domains;
